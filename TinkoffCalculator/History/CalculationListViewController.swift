c18718abf8c742957da59a82f075ebd5dd8131d3
Bokhulenkov <bokhulenkov@gmail.com>
2024-04-18 17:26:42 +0300

    reuse cell

--- TinkoffCalculator/History/CalculationListViewController.swift
+++ TinkoffCalculator/History/CalculationListViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CalculationListViewContoller: UIViewController {
     
+    //    MARK: - Properties
+    
     var calculations: [(expression: [CalculationHistoryItem], result: Double)] = []
     
     @IBOutlet weak var calculationLabel: UILabel!
@@ -33,19 +35,25 @@ class CalculationListViewContoller: UIViewController {
     }
     
     override func viewDidLoad() {
-         super.viewDidLoad()
+        super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
         
-//        регистрируем ячейку таблицы
+        tableView.backgroundColor = UIColor.systemGray5
+        let tableHeaderView = UIView()
+        tableHeaderView.frame = CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 30)
+        tableView.tableHeaderView = tableHeaderView
+        tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 1))
+        
+        //        регистрируем ячейку таблицы
         let nib = UINib(nibName: "HistoryTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "HistoryTableViewCell")
     }
     
     //    MARK: - Helpers
     
-//     кнопка закрыть
+    //     кнопка закрыть
     @IBAction func dismissVC(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
@@ -53,20 +61,49 @@ class CalculationListViewContoller: UIViewController {
     private func initialize() {
         modalPresentationStyle = .fullScreen
     }
+    
+//    принимаем выражение и отображаем строку
+    private func expressionsToString(_ expressions: [CalculationHistoryItem]) -> String {
+        var result = ""
+        
+        for operand in expressions {
+            switch operand {
+            case let .number(value):
+                result += String(value) + " "
+            case let .operation(value):
+                result += value.rawValue + " "
+                
+            }
+        }
+        return result
+    }
+    
 }
 
+// MARK: - Extension
+
+
 extension CalculationListViewContoller: UITableViewDelegate {
+//    высота ячейки
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90.0
+    }
     
 }
 
 extension CalculationListViewContoller: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return calculations.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        //        получаем ячейку
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryTableViewCell", for: indexPath) as! HistoryTableViewCell
+//        номер строки ячейки
+        let historyItem = calculations[indexPath.row]
+        cell.configure(with: expressionsToString(historyItem.expression), result: String(historyItem.result))
+        return cell
         
     }
     
